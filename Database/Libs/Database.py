d1a15afa5119f1e9e4092e61e8228cf3df894617
HugoSerra <tcarvalhotaichou@gmail.com>
2023-04-23 19:29:50 -0300

    Leitura e escrita fixas, bugs corrigidos

--- Database/Libs/Database.py
+++ Database/Libs/Database.py
@@ -5,22 +5,25 @@ class Tools:
     def set_size(self,str,size):
         return f"{str}" + (size-len(str)-1) * " "
 
-
 class Register(Tools):
 
     def __init__(self):
-        pass
+        self.size = {}
 
     def set_attribute(self,name,value,size=None):
         self.__dict__[name] = value if(size == None) else self.set_size(value,size)
+        self.size[name] = len(value)
 
     def get_attributes(self):
-        return self.__dict__
+        dict = self.__dict__.copy()
+        dict.pop('size')
+        return dict
 
 class Database(Tools):
 
     def __init__(self):
         self.database = DatabaseFile("Metadados/games.txt")
+        self.register = Register()
         self.register_size = {}
 
     def validate(self):
@@ -33,28 +36,30 @@ class Database(Tools):
         self.database = DatabaseFile(archive_name)
         self.database.validate()
 
-    def set_fixed_register(self,register):
+    def set_fixed_register(self):
 
         register_fixed_size = ""
-        for key, value in register.get_attributes().items():
+        for key, value in self.register.get_attributes().items():
             self.register_size[key] = len(value)
             register_fixed_size += value
 
         self.database.write(f"{register_fixed_size}\n")
 
-    def get_fixed_registers(self):
+    def get_fixed_registers(self,register_size=None):
+        if(register_size != None):
+            self.register_size = register_size
+
         self.database.pointer_reset()
         registers = []
 
         for str_register in self.database.archive.readlines():
             register = Register()
             pointer = 0
-
             for attribute, size in self.register_size.items():
                 register.set_attribute(attribute, str(str_register[pointer:pointer+size]).strip())
                 pointer += size
 
-            registers.append(register.__dict__)
+            registers.append(register.get_attributes())
 
         return registers
 
--- Database/main.py
+++ Database/main.py
@@ -3,20 +3,19 @@ from Libs.Database import Database, Register
 DB = Database().validate()
 DB.set_database_archive("Metadados/DB.txt")
 
-register = Register()
-register.set_attribute('nome','The Wither 2', 50)
-register.set_attribute('Preco','150', 5)
-register.set_attribute('Categoria','Medieval', 15)
-register.set_attribute('Idade','15', 3)
-DB.set_fixed_register(register)
+DB.register.set_attribute('nome','The Wither 2', 50)
+DB.register.set_attribute('Preco','150', 5)
+DB.register.set_attribute('Categoria','Medieval', 15)
+DB.register.set_attribute('Idade','15', 3)
+DB.set_fixed_register()
 
-register.set_attribute('nome','Assassins Creed', 50)
-register.set_attribute('Preco','139', 5)
-register.set_attribute('Categoria','Medieval', 15)
-register.set_attribute('Idade','19', 3)
-DB.set_fixed_register(register)
+DB.register.set_attribute('nome','Assassins Creed',50)
+DB.register.set_attribute('Preco','139',5)
+DB.register.set_attribute('Categoria','Medieval',15)
+DB.register.set_attribute('Idade','19',3)
+DB.set_fixed_register()
 
-registers = DB.get_fixed_registers()
+registers = DB.get_fixed_registers({"nome":49, "Preco":4, "Categoria":14, "Idade":2})
 print(registers)
 
 
